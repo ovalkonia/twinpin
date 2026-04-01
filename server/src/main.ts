eb59540b6e0747d50feed60da02be636d0064f78
ovalkonia <ovalkonia@gmail.com>
2026-04-01 09:20:01 +0300

    feat(server): cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,6 +8,10 @@ async function bootstrap() {
     whitelist: true,
     transform: true,
   }));
+  app.enableCors({
+    origin: process.env.URL_FRONTEND,
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 
